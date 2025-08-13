6db8b885736d39115f002d73d3d487388a6126f9
Jacrz07 <jeffersonchandias07@gmail.com>
2025-08-13 17:37:30 -0600

    fixing validate_catalog_is_assigned_pipeline

--- pipelines/catalog_pipelines.py
+++ pipelines/catalog_pipelines.py
@@ -105,7 +105,7 @@ def get_discount_catalogs_pipeline() -> list:
         }}
     ]
 
-def validate_catalog_is_assigned_pipeline() -> list:
+def validate_catalog_is_assigned_pipeline(id) -> list:
     return [
         {
             "$match": {
